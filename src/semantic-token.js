0b87de37c954e05f41e516a1cc54912c243dc9e2
peakchen90 <1558267774@qq.com>
2021-11-25 17:18:12 +0800

    +

---
+++ src/semantic-token.js
@@ -0,0 +1,31 @@
+const tokenTypes = ['class', 'interface', 'enum', 'function', 'variable'];
+const tokenModifiers = ['declaration', 'documentation'];
+const legend = new vscode.SemanticTokensLegend(tokenTypes, tokenModifiers);
+
+/**
+ * @type {import('vscode').DocumentSemanticTokensProvider}
+ */
+const provider = {
+  /**
+   * @param {import('vscode').TextDocument} document
+   * @returns
+   */
+  provideDocumentSemanticTokens(document) {
+    // analyze the document and return semantic tokens
+
+    // TODO: 解析 Token，并标记
+
+    const tokensBuilder = new vscode.SemanticTokensBuilder(legend);
+    // on line 1, characters 1-5 are a class declaration
+    tokensBuilder.push(
+      new vscode.Range(new vscode.Position(1, 1), new vscode.Position(1, 5)),
+      'class',
+      ['declaration']
+    );
+    return tokensBuilder.build();
+  }
+};
+
+const selector = { language: 'x-lang', scheme: 'file' }; // register for all Java documents from the local file system
+
+vscode.languages.registerDocumentSemanticTokensProvider(selector, provider, legend);
